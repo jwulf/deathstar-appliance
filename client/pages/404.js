7c00a9e8ec3aac04909805c29b59cd0020e8b1c7
Sitapati <sitapati@worldsankirtan.net>
2012-11-05 00:39:58 +1100

    remote updating

--- client/pages/404.js
+++ client/pages/404.js
@@ -4,18 +4,7 @@ if (Meteor.is_client) {
 
 	Template.page_404.error_message = function () {
 				
-		if(parameters.msg){
-			var msg =  parameters.msg;
-		}
-		else {
-			if(message_404){
-				var msg =  message_404;
-			}
-			else {
-				var msg = "Page not found";
-			}	
-		}
-		return msg;
+		return 'Page not found';
 	}
 
 
--- server/server.js
+++ server/server.js
@@ -15,15 +15,45 @@ Meteor.startup(function () {
 
 });   
 
+function updateFromURL(url) {
+    // Stub function
+    return Meteor.call('updateFromURL', url);
+}
+
+function processConfigurationObject( resultJSON, req_dryrun) {
+    // Stub function
+    return Meteor.call('processConfigurationObject', resultJSON, req_dryrun);
+}
+
 Meteor.methods({
+    
+    // Allow initial configuration via remote method call
+    initialize: function(url){
         
+        // First, we check for existing configuration information
+        // This is a one-time operation        
+        if (Config.find({}).count() !== 0) return ({error: 2, msg: 'This machine has already been initialized. Initialization is a one-time operation. For further configuration use manual configuration or remote updates.'});
+        
+        var result = updateFromURL(url);
+        if (!result.error)  
+            Config.insert({initialized: 'remotely', date: humanDate(), remoteInitializationURL : url});
+        
+        return result;
+            
+    },
+    
     // Method exposed to allow rapid prototyping and troubleshooting in the field
     // Should be protected in a production model
     updateFromURL: function(url, req_dryrun){
         var result, resultJSON;
-        console.log( 'Fetching update from ' + url );
+        console.log( 'Requested update from ' + url );
+        
+        // Called with no URL
+        if (!url) return ({error: true, msg: 'No URL passed for update'});
+        
         var result = Meteor.http.get(url);
         if (result.statusCode === 200)
+        {
             console.log('Got a result');
             if ( result.data ) {
                 console.log('Response is JSON-encoded data');
@@ -42,14 +72,16 @@ Meteor.methods({
                    catch (e)
                    {
                         console.log('Error parsing response object');
-                        return('Error parsing response object');
+                        return({error: true, msg: 'I couldn\'t understand the configuration instructions retrieved from ' + url});
                    }
             }
             if (resultJSON) {
                 var status = Meteor.call('processConfigurationObject',resultJSON, req_dryrun);
                 return(status);
             }
-        return(result);
+            return({error: 'error', msg: 'No update information found at ' + url});
+        }
+        return({error: 'error', msg: 'I encountered an error while attempting to retrieve configuration instructions from: ' + url});
     },
     
     processConfigurationObject: function (configurationObject, req_dryrun){
@@ -58,6 +90,10 @@ Meteor.methods({
         const CMD_COPY_FILE = 'copy file';
         const CMD_PULL_UPDATE = 'pull update';
         const CMD_DATA = 'data';
+        
+        var result = {};
+        result.originalJSON = configurationObject;
+        result.date = humanDate();
         
         console.log('Processing configuration commands:');
         //console.log(configurationObject);
@@ -81,7 +117,8 @@ Meteor.methods({
                 human_readable_task_name_2:
                 {
                     "action": "data",
-                    "domain" : [system, csprocessor, appliance]
+                    "domain" : [system | csprocessor | appliance],
+                    "operation" : [set | unset ],
                     "key": "someDBfield",
                     "value": "somevalue"
                 },
@@ -116,7 +153,7 @@ Meteor.methods({
             if (InstalledUpdates.find({uuid: configurationObject.uuid}).count > 0 ){
                 console.log("This update has already been applied to this system");
                 if (!dryrun)
-                    return('Update has already been applied');
+                    return({error: 1, msg: 'Update has already been applied'});
             }
               
             if (dryrun) console.log('Tasks:');
@@ -132,9 +169,11 @@ Meteor.methods({
                 
                 if (task.action && task.action == CMD_COPY_FILE) {
                     if (task.src && task.dest) {
-                        if (dryrun) console.log( '' + numerator + '. ' + item +
-                            '\nCopy ' + task.src + ' to ' + task.dest + '\n');
+                        if (dryrun) result['task_' + numerator] = '"'+ item + '" :' +
+                            '\nCopy ' + task.src + ' to ' + task.dest + '\n';
+                            
                         if (!dryrun) Meteor.call('copyFile', task.src, task.dest);       
+                        
                         numerator ++;
                     }
                 }
@@ -145,26 +184,49 @@ Meteor.methods({
                 //
                 
                 if (task.action && task.action == CMD_DATA){
-                    if (task.key && task.value) {
+                    if (task.key && task.value && task.operation) {
                         
-                        if (dryrun) console.log('' + numerator + '. ' + item + 
-                            '\nSet ' + task.domain + 
-                            ':' + task.key + ' to ' + task.value + '\n');
+                        if (dryrun) result['task_' + numerator] = '"' + item + '" :' + 
+                            '\n' + task.operation +'  ' + task.domain + 
+                            '/' + task.key + ' to ' + task.value + '\n';
                         numerator ++;
                         if (!dryrun) {
                             var isNew = false;
                             var updateObject = {};
-                            record = Config.find({domain: task.domain});
-                            if (record.count() == 0) {
-                                // initializing this domain
+                            
+                            if ( task.operation == "set" ) {
+                                    
+                                console.log('find({ domain: ' + task.domain+'})');
+                                record = Config.find({domain: task.domain}).fetch();
+                                if (record.length === 0) {
+                                    // initializing this domain
+                                    console.log('Initializing domain: ' + task.domain);
+                                    updateObject.domain = task.domain;
+                                    updateObject[task.key] = task.value;
+                                    console.log(updateObject);
+                                    console.log('Config.insert(');
+                                    console.log(updateObject);
+                                    Config.insert(updateObject);
+                                } else {
+                                    // updating this domain
+                                    console.log('Updating domain: ' + task.domain);
+                                    updateField = {};
+                                    updateField[task.key] = task.value;
+                                    updateObject = { $set: updateField };
+                                    console.log('Config.update({domain : ' + task.domain+'},');
+                                    console.log(updateObject);
+                                    Config.update({domain: task.domain}, updateObject);
+                                }
+                            }
+                            
+                            // Check this out, because I think this will delete the
+                            // whole document
+                            if ( task.operation == 'unset' ) {
                                 updateObject.domain = task.domain;
                                 updateObject[task.key] = task.value;
-                                Config.insert(updateObject);
-                            } else {
-                                // updating this domain
-                                updateObject[task.key] = {" $set": "value" }
-                                Config.update({'domain': task.domain}, updateObject);
+                                record = Config.remove(updateObject);   
                             }
+                            
                         }                    
                     }                    
                 }
@@ -176,8 +238,8 @@ Meteor.methods({
                 
                 if (task.action && task.action == CMD_INSTALL_PACKAGE){
                     if (task.pkg) {
-                        if (dryrun) console.log('' + numerator + '. ' + item + 
-                            '\nInstall Package: ' + task.pkg + '\n');
+                        if (dryrun) result['task_' + numerator] = '"' + item + '" :' + 
+                            '\nInstall Package ' + task.pkg + '\n';
                         if (!dryrun)
                             Meteor.call('installPackage',task.pkg);   
                         numerator ++;
@@ -190,7 +252,7 @@ Meteor.methods({
                 //
                 
                 if (task.action && task.action == CMD_PULL_UPDATE){
-                    if (dryrun) console.log('' + numerator + '. ' + '\nPull Git update\n');
+                    if (dryrun) result['task_' + numerator] = '\nPull Git update\n';
                     if (!dryrun) Meteor.call('pullUpdate');
                     numerator ++;
                 }
@@ -206,8 +268,10 @@ Meteor.methods({
         // TODO: Look at making it async using a Fiber
         
         if ( !dryrun ) {
-            InstalledUpdates.insert(configurationObject);
-            return ('Update applied'); 
+            InstalledUpdates.insert(result);
+            return ({error: false, result: result, msg: 'Update applied'}); 
+        } else {
+            return ({error: false, result: result, msg: 'Dry run completed'});
         }
     },
     
@@ -250,10 +314,21 @@ Meteor.methods({
         });
     }
     
-    
-    
-    
 });
+
+function humanDate() {
+    var date = new Date();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];  
+    var months = ["January", "February", "March", "April", "May",   
+        "June", "July", "August", "September", "October", "November", "December"];  
+    var pad = function(str) { str = String(str); return (str.length < 2) ? "0" + str : str; }  
+  
+    var meridian = (parseInt(date.getHours() / 12) == 1) ? 'PM' : 'AM';  
+    var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();  
+    return days[date.getDay()] + ' ' + months[date.getMonth()] + ' ' + date.getDate() + ' '   
+        + date.getFullYear() + ' ' + hours + ':' + pad(date.getMinutes()) + ':'   
+        + pad(date.getSeconds()) + ' ' + meridian;  
+}  
 
 
 
